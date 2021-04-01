7d8fcfc57294a65ac02b23b2956b3e25ab3afe15
christian <christiankamdem2@gmail.com>
2021-04-01 11:05:28 -0700

    pAge services (incompleted)

--- pages/services/index.js
+++ pages/services/index.js
@@ -1,11 +1,330 @@
 import React from 'react';
+import { useState } from 'react';
 import Public from '../../layouts/Public';
-import Jumbotron from 'react-bootstrap/Jumbotron';
+import { TabContent, TabPane, Nav, CardImg, CardBody, NavItem, NavLink, Card, Button, CardTitle, CardText, Row, Col } from 'reactstrap';
+import classnames from 'classnames';
+import { Media } from 'reactstrap';
+import { Jumbotron, Container } from 'reactstrap';
+import {
+  Carousel,
+  CarouselItem,
+  CarouselControl,
+  CarouselIndicators,
+  CarouselCaption
+} from 'reactstrap';
 
 function Services () {
+	//Slider
+	const items = [
+		  {
+		    id: 1,
+		    altText: 'Slide 1',
+		    caption: 'Slide 1'
+		  },
+		  {
+		    id: 2,
+		    altText: 'Slide 2',
+		    caption: 'Slide 2'
+		  },
+		  {
+		    id: 3,
+		    altText: 'Slide 3',
+		    caption: 'Slide 3'
+		  }
+		];
+		const items3 = [
+		  {
+		    id: 1,
+		    altText: 'Slide 1',
+		    caption: 'Slide 1'
+		  },
+		  {
+		    id: 2,
+		    altText: 'Slide 2',
+		    caption: 'Slide 2'
+		  },
+		  {
+		    id: 3,
+		    altText: 'Slide 3',
+		    caption: 'Slide 3'
+		  }
+		];
+	  const [activeIndex, setActiveIndex] = useState(0);
+	  const [animating, setAnimating] = useState(false);
+
+	  const next = () => {
+	    if (animating) return;
+	    const nextIndex = activeIndex === items.length - 1 ? 0 : activeIndex + 1;
+	    setActiveIndex(nextIndex);
+	  }
+
+	  const previous = () => {
+	    if (animating) return;
+	    const nextIndex = activeIndex === 0 ? items.length - 1 : activeIndex - 1;
+	    setActiveIndex(nextIndex);
+	  }
+
+	  const goToIndex = (newIndex) => {
+	    if (animating) return;
+	    setActiveIndex(newIndex);
+	  }
+
+	  const slides = items.map((item) => {
+	    return (
+	      <CarouselItem
+	        className="custom-tag"
+	        tag="div"
+	        key={item.id}
+	        onExiting={() => setAnimating(true)}
+	        onExited={() => setAnimating(false)}
+	      >
+	      	  <Row>
+		        <Col xs="6">
+		        	<Jumbotron fluid className="services_page_section_presentation_jombotron">
+				        <Container fluid>
+				          <h1>Participer au pool de liquidité</h1>
+				          <p className="lead">Généralement, on utilise un texte en faux latin (le texte ne veut rien dire, il a été modifié), le Lorem ipsum ou Lipsum. L'avantage du latin est que l'opérateur sait au premier coup d'œil</p>
+				          <p className="lead">Généralement, on utilise un texte en faux latin (le texte ne veut rien dire, il a été modifié), le Lorem ipsum ou Lipsum. L'avantage du latin est que l'opérateur sait au premier coup d'œil</p>
+				          <h4>LEARN MORE</h4><img src="../../../../assets/img/theme/profile-cover.jpg"/>
+				        </Container>
+				      </Jumbotron>
+		        </Col>
+		        <Col xs="6">
+		        	<Jumbotron fluid className="services_page_section_presentation_jombotron">
+				        <Container fluid>
+				          <img src="/img/theme/profile-cover.jpg"/>
+				        </Container>
+				      </Jumbotron>
+		        </Col>
+		      </Row>
+	      </CarouselItem>
+	    );
+	  });
+	  const slides3 = items3.map((item) => {
+	    return (
+	      <CarouselItem
+	        className="custom-tag"
+	        tag="div"
+	        key={item.id}
+	        onExiting={() => setAnimating(true)}
+	        onExited={() => setAnimating(false)}
+	      >
+	      	  <Row>
+		        <Col xs="6">
+		        	<Card>
+				        <CardImg top width="100%" src="/assets/318x180.svg" alt="Card image cap" />
+				        <CardBody>
+				          <CardTitle tag="h5">Card Title</CardTitle>
+				          <CardText>This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</CardText>
+				          <CardText>
+				            <small className="text-muted">Last updated 3 mins ago</small>
+				          </CardText>
+				        </CardBody>
+				      </Card>
+		        </Col>
+		        <Col xs="6">
+		        	<Card>
+				        <CardImg top width="100%" src="/assets/318x180.svg" alt="Card image cap" />
+				        <CardBody>
+				          <CardTitle tag="h5">Card Title</CardTitle>
+				          <CardText>This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</CardText>
+				          <CardText>
+				            <small className="text-muted">Last updated 3 mins ago</small>
+				          </CardText>
+				        </CardBody>
+				      </Card>
+		        </Col>
+		      </Row>
+	      </CarouselItem>
+	    );
+	  });
+	//
+  const [activeTab, setActiveTab] = useState('1');
+  const toggle = tab => {
+    if(activeTab !== tab) setActiveTab(tab);
+  }
   return (
     <div>
       	<div className="services_page_section_image">
+	    </div>
+	    <div className="services_page_section_presentation">
+	    	<h1>Présentation Crowndlending</h1>
+		      <Nav tabs>
+			        <NavItem className="services_page_section_presentation_tab">
+			          <NavLink
+			            className={classnames({ active: activeTab === '1' })}
+			            onClick={() => { toggle('1'); }}
+			          >
+			            Participation au pool de liquidité
+			          </NavLink>
+			        </NavItem>
+			        <NavItem className="services_page_section_presentation_tab">
+			          <NavLink
+			            className={classnames({ active: activeTab === '2' })}
+			            onClick={() => { toggle('1'); }}
+			          >
+			            Gestion, acquisition de produits numériques
+			          </NavLink>
+			         </NavItem> 
+		           <NavItem className="services_page_section_presentation_tab">
+			          <NavLink
+			            className={classnames({ active: activeTab === '3' })}
+			            onClick={() => { toggle('1'); }}
+			          >
+			            Monitoring et suivie des positions de participation
+			          </NavLink>
+		          </NavItem>
+		          <NavItem className="services_page_section_presentation_tab">
+			          <NavLink
+			            className={classnames({ active: activeTab === '3' })}
+			            onClick={() => { toggle('1'); }}
+			          >
+			            Partage des intérêts
+			          </NavLink>
+		          </NavItem> 
+		           <NavItem className="services_page_section_presentation_tab">
+			          <NavLink
+			            className={classnames({ active: activeTab === '4' })}
+			            onClick={() => { toggle('1'); }}
+			          >
+			            Satisfactions des membres de la communauté
+			          </NavLink>
+		        	</NavItem>
+		      </Nav>
+	      <TabContent activeTab={activeTab}>
+	        <TabPane tabId="1">
+	          <Row>
+	            <Col sm="12">
+		            <Media left href="#">
+			          <Media object data-src="./assets/login.png"/>
+			        </Media>
+	              	 <style>
+				        {
+				          `.custom-tag {
+				              max-width: 100%;
+				              height: 500px;
+				              background: transparent;
+				            }`
+				        }
+				      </style>
+				      <Carousel
+				        activeIndex={activeIndex}
+				        next={next}
+				        previous={previous}
+				      >
+				        <CarouselIndicators items={items} activeIndex={activeIndex} onClickHandler={goToIndex} />
+				        {slides}
+				      </Carousel>
+	            </Col>
+	          </Row>
+	        </TabPane>
+	        <TabPane tabId="2">
+	          <Row>
+	            <Col sm="6">
+	              <Card body>
+	                <CardTitle>Special Title Treatment</CardTitle>
+	                <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
+	                <Button>Go somewhere</Button>
+	              </Card>
+	            </Col>
+	            <Col sm="6">
+	              <Card body>
+	                <CardTitle>Special Title Treatment</CardTitle>
+	                <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
+	                <Button>Go somewhere</Button>
+	              </Card>
+	            </Col>
+	          </Row>
+	        </TabPane>
+	      </TabContent>
+	    </div>
+	    <div className="services_page_section_networking">
+	    	<h1>Présentation Networking</h1>
+			    	   <Nav tabs>
+					        <NavItem className="services_page_section_presentation_tab">
+					          <NavLink
+					            className={classnames({ active: activeTab === '1' })}
+					            onClick={() => { toggle('1'); }}
+					          >
+					            Notre système d'affiliation
+					          </NavLink>
+					        </NavItem>
+					        <NavItem className="services_page_section_presentation_tab">
+					          <NavLink
+					            className={classnames({ active: activeTab === '2' })}
+					            onClick={() => { toggle('1'); }}
+					          >
+					            Plan de compensation
+					          </NavLink>
+					         </NavItem> 
+				           <NavItem className="services_page_section_presentation_tab">
+					          <NavLink
+					            className={classnames({ active: activeTab === '3' })}
+					            onClick={() => { toggle('1'); }}
+					          >
+					            La licence
+					          </NavLink>
+				          </NavItem>
+				      </Nav>
+			      <TabContent activeTab={activeTab}>
+			        <TabPane tabId="1">
+			          <Row>
+			            <Col sm="12">
+				            <Media left href="#">
+					          <Media object data-src="./assets/login.png"/>
+					        </Media>
+			              	 <style>
+						        {
+						          `.custom-tag {
+						              max-width: 100%;
+						              height: 500px;
+						              background: transparent;
+						            }`
+						        }
+						      </style>
+						      <Carousel
+						        activeIndex={activeIndex}
+						        next={next}
+						        previous={previous}
+						      >
+						        <CarouselIndicators items={items} activeIndex={activeIndex} onClickHandler={goToIndex} />
+						        {slides}
+						        <CarouselControl direction="prev" directionText="Previous" onClickHandler={previous} />
+						        <CarouselControl direction="next" directionText="Next" onClickHandler={next} />
+						      </Carousel>
+			            </Col>
+			          </Row>
+			        </TabPane>
+			        <TabPane tabId="2">
+			          <Row>
+			            <Col sm="6">
+			              <Card body>
+			                <CardTitle>Special Title Treatment</CardTitle>
+			                <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
+			                <Button>Go somewhere</Button>
+			              </Card>
+			            </Col>
+			            <Col sm="6">
+			              <Card body>
+			                <CardTitle>Special Title Treatment</CardTitle>
+			                <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
+			                <Button>Go somewhere</Button>
+			              </Card>
+			            </Col>
+			          </Row>
+			        </TabPane>
+			      </TabContent>
+	    </div>
+	    <div className="services_page_section_partenaires">
+	    	<h1>Partenaires</h1>
+	    	<Carousel
+				        activeIndex={activeIndex}
+				        next={next}
+				        previous={previous}
+				      >
+				        <CarouselIndicators items={items} activeIndex={activeIndex} onClickHandler={goToIndex} />
+				        {slides3}
+				      </Carousel>
 	    </div>
     </div>
   )
