753ff4961a6743e78320e2af1b9ec3b62c9662c7
toumi maximilien <itguymax@gmail.com>
2021-04-08 12:34:12 +0100

    redirect to preinscription

--- pages/auth/pre-inscription.js
+++ pages/auth/pre-inscription.js
@@ -1,21 +1,19 @@
 import React, { Component } from "react";
 import Router from "next/router";
-import {css} from '@emotion/react'
-import Pi from "../../src/components/pre_inscription"
+import {css} from '@emotion/react';
+import { Container } from "reactstrap";
+import Image from "next/image";
+import Pi from "../../src/components/pre_inscription";
 export default function PreInscription() {
   return (
     <div>
-      <div
-        className="mb-5"
-        style={{
-          minHeight: "600px",
-          backgroundImage: 
-             "url(" + "/assets/img/enconstruction.png" + ")",
-          backgroundSize: "cover",
-          backgroundPosition: "center top",
-        }}
-      >
-       
+      <div style={{width:"100%"}}>
+        <Container className=" d-flex justify-content-center aling-items-center" fluid style = {{
+              minHeight: "600px",
+            }}> 
+          <Image src= "/assets/img/enconstruction.png" priority={true}  layout="fill"  className=" "/>
+          
+        </Container>
       </div>
       <Pi bleu/>
     </div>
