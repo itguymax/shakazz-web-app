68f99bd4854cba3b6dc5da304c0101b52d17924b
toumi maximilien <itguymax@gmail.com>
2021-04-27 15:22:22 +0100

    api pattern

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,52 +1,35 @@
 
+import  apiV1  from './config';
 
-const apiUrl = "https://shakazz-server.herokuapp.com/api/v1/services/auth"
-
-  const signup = async (data) => {
+  const signupUser = async (data) => {
+    const url = '/signup';
+    const token = "";
     try {
-    let response = await fetch(`${apiUrl}/signup`, {
-      method: 'POST',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(data)
-    });
-    let responseJson =  await response.json();
+       let responseJson = await  apiV1.postJson(url,token, data);
     return responseJson;
   } catch(err) {
+     console.log(err.message);
+  }
+}
   
-  }
-  }
-
-  const login = async (data) => {
+const loginUser = async (data) => {
+    const url = "/login";
+    const token ="";
     try {
-    let response = await fetch(`${apiUrl}/login`, {
-      method: 'POST',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(data)
-    });
-    let responseJson =  await response.json();
+    let responseJson =  apiV1.postJson(url,token, data,);
     return responseJson;
   } catch(err) {
-    console.log(err)
+    console.log(err);
   }
   }
 
-  const requestPasswordReset = async (data) => {
+
+  const requestUserPasswordReset = async (data) => {
+    const url = "/requestPasswordReset";
+    const token = "";
     try {
-    let response = await fetch(`${apiUrl}/requestPasswordReset`, {
-      method: 'POST',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(data)
-    });
-    let responseJson =  await response.json();
+  
+    let responseJson =  apiV1.postJson(url, token, data);
     return responseJson;
   } catch(err) {
     console.log(err)
@@ -54,17 +37,12 @@ const apiUrl = "https://shakazz-server.herokuapp.com/api/v1/services/auth"
   }
 
 
-  const resetPassword = async (data) => {
+  const resetUserPassword = async (data) => {
+    const url = "/resetPassword";
+    const token = "token";
     try {
-    let response = await fetch(`${apiUrl}/resetPassword`, {
-      method: 'POST',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(data)
-    });
-    let responseJson =  await response.json();
+   
+    let responseJson =  await apiV1.postJson(url, token, data);
     return responseJson;
   } catch(err) {
     console.log(err)
@@ -72,8 +50,8 @@ const apiUrl = "https://shakazz-server.herokuapp.com/api/v1/services/auth"
   }
 
 export {
-  signup,
-  login,
-  requestPasswordReset,
-  resetPassword,
+  signupUser,
+  resetUserPassword,
+  loginUser,
+  requestUserPasswordReset,
 };
---
+++ src/services/config.js
@@ -0,0 +1,130 @@
+// acepted requests format
+const ACCEPT = {
+  JSON: 'application/json',
+};
+
+// api verbs
+const METHOD = {
+  GET: 'GET',
+  HEAD: 'HEAD',
+  PUT: 'PUT',
+  DELETE: 'DELETE',
+  POST: 'POST',
+};
+
+const apiBaseUrl = "https://shakazz-server.herokuapp.com/api/v1/services";
+// shakkazz api pattern
+  const apiV1 = {
+  root: apiBaseUrl,
+  call: async (url, parameters) => {
+    const finalUrl =
+      url.indexOf(apiV1.root) === 0 ? url : url.startsWith('/')?`${apiV1.root}${url}`: `${apiV1.root}/${url}`;
+    const response = await fetch(finalUrl, parameters);
+    return response;
+  },
+  parameters: (
+    accessToken,
+    method = METHOD.GET,
+    accept = ACCEPT.JSON,
+    body = {},
+  ) => {
+    const withBody = [METHOD.PUT, METHOD.PATCH, METHOD.POST];
+    const params = {
+      method,
+      headers: {
+        Accept: accept,
+        auth_token: `${accessToken}`,
+        'Content-Type': 'application/json',
+        'Cache-Control': 'no-cache',
+      },
+    };
+
+    if (withBody.indexOf(method) !== -1) {
+      params.body = JSON.parse(body);
+      if (method === METHOD.PUT) {
+        params.headers['Content-Length'] = 0;
+      }
+    }
+
+    return params;
+  },
+  unAuthParameters: (method = METHOD.POST, accept = ACCEPT.JSON, body = {}) => {
+    const withBody = [METHOD.PUT, METHOD.PATCH, METHOD.POST];
+    const params = {
+      method,
+      headers: {
+        Accept: accept,
+        'Content-Type': 'application/json',
+      },
+    };
+
+    if (withBody.indexOf(method) !== -1) {
+      params.body = JSON.parse(body);
+      if (method === METHOD.PUT) {
+        params.headers['Content-Length'] = 0;
+      }
+    }
+
+    return params;
+  },
+  delete: async (url, accessToken) => {
+    const response = await  apiV1.call(
+      url,
+       apiV1.parameters(accessToken, METHOD.DELETE),
+    );
+
+    return response;
+  },
+  get: async (url, accessToken) => {
+    const response = await  apiV1.call(
+      url,
+       apiV1.parameters(accessToken),
+    );
+
+    return response;
+  },
+  getJson: async (url, accessToken) => {
+    const response = await  apiV1.call(
+      url,
+       apiV1.parameters(accessToken),
+    );
+
+    return response.json();
+  },
+  postJson: async (url, accessToken, body = {}) => {
+    const response = await  apiV1.call(
+      url,
+       apiV1.parameters(accessToken, METHOD.POST, ACCEPT.JSON, body),
+    );
+
+    return response.json();
+  },
+  post: async (url, accessToken, body = {}) => {
+    const response = await  apiV1.call(
+      url,
+      apiV1.parameters(accessToken, METHOD.POST, ACCEPT.JSON, body),
+    );
+
+    return response;
+  },
+  put: async (url, accessToken, body = {}) => {
+    const response = await  apiV1.call(
+      url,
+       apiV1.parameters(accessToken, METHOD.PUT, ACCEPT.JSON, body),
+    );
+
+    return response;
+  },
+  putJson: async (url, accessToken, body = {}) => {
+    const response = await  apiV1.call(
+      url,
+       apiV1.parameters(accessToken, METHOD.PUT, ACCEPT.JSON, body),
+    );
+    return response.json();
+  },
+};
+
+
+
+
+export default apiV1;
