c766ea2383983aa8b198de0223b754971e3ce6e6
toumi maximilien <itguymax@gmail.com>
2021-06-14 16:33:12 +0100

    change post to put json on update user profile

--- src/services/profileUserInfos.service.js
+++ src/services/profileUserInfos.service.js
@@ -6,7 +6,7 @@ import  apiV1  from './config';
 const profileUserInfos = async ({accessToken, data}) => {
     const url = `/users/update`;
     try {
-    let responseJson =  apiV1.postJson(url,accessToken,data);
+    let responseJson =  apiV1.putJson(url,accessToken,data);
     return responseJson;
   } catch(err) {
     console.log(err);
